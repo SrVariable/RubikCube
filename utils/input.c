5d332572d72ed96eab90c85c06f8ae98d9001b6b
SrRecursive <rojohnibana@gmail.com>
2023-12-21 15:16:38 +0100

    Specified how to exit the program

--- utils/input.c
+++ utils/input.c
@@ -5,7 +5,7 @@ int	user_input(char ****cube)
 	char	input;
 	int		status;
 
-	printf("Introduce a movement (B, F, L, R, U, D): ");
+	printf("Introduce a movement (B, F, L, R, U, D) or exit (E, Q): ");
 	scanf("%c", &input);
 	getchar();
 	status = handle_movement(cube, toupper(input));
