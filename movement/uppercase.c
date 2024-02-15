2cafff2576426dc4fa65930945855d20cf99adb5
SrVariable <rojohnibana@gmail.com>
2024-02-15 14:47:26 +0100

    Fix wrong rotation direction

--- movement/uppercase.c
+++ movement/uppercase.c
@@ -21,18 +21,17 @@ void	move_back(char ****cube)
 		}
 	}
 	for (int i = 0; i < 12; i++)
-		temp[i] = (*cube)[PURPLE][0][i];
+		temp[i] = (*cube)[YELLOW][i / 4][i % 4];
 	for (int i = 0; i < 4; i++)
 	{
-		(*cube)[PURPLE][0][i] = (*cube)[PURPLE][2][i];
-		(*cube)[PURPLE][0][i + 4] = (*cube)[PURPLE][1][i];
-		(*cube)[PURPLE][1][i] = (*cube)[PURPLE][2][i + 4];
-		(*cube)[PURPLE][2][i] = (*cube)[PURPLE][2][i + 8];
-		(*cube)[PURPLE][2][i + 4] = (*cube)[PURPLE][1][i + 8];
+		(*cube)[YELLOW][0][i] = (*cube)[YELLOW][0][i + 8];
+		(*cube)[YELLOW][1][i] = (*cube)[YELLOW][0][i + 4];
+		(*cube)[YELLOW][0][i + 4] = (*cube)[YELLOW][1][i + 8];
+		(*cube)[YELLOW][0][i + 8] = (*cube)[YELLOW][2][i + 8];
+		(*cube)[YELLOW][1][i + 8] = (*cube)[YELLOW][2][i + 4];
 	}
-	for (int i = 0; i < ROW; i++)
-		for (int j = 0; j < 4; j++)
-			(*cube)[PURPLE][i][j + 8] = temp[(i + 1) * 4 + j - 4];
+	for (int i = 0; i < 12; i++)
+		(*cube)[YELLOW][2][i] = temp[i];
 }
 
 /**
@@ -56,17 +55,18 @@ void	move_front(char ****cube)
 		}
 	}
 	for (int i = 0; i < 12; i++)
-		temp[i] = (*cube)[YELLOW][i / 4][i % 4];
+		temp[i] = (*cube)[YELLOW][0][i];
 	for (int i = 0; i < 4; i++)
 	{
-		(*cube)[YELLOW][0][i] = (*cube)[YELLOW][0][i + 8];
-		(*cube)[YELLOW][1][i] = (*cube)[YELLOW][0][i + 4];
-		(*cube)[YELLOW][0][i + 4] = (*cube)[YELLOW][1][i + 8];
-		(*cube)[YELLOW][0][i + 8] = (*cube)[YELLOW][2][i + 8];
-		(*cube)[YELLOW][1][i + 8] = (*cube)[YELLOW][2][i + 4];
+		(*cube)[YELLOW][0][i] = (*cube)[YELLOW][2][i];
+		(*cube)[YELLOW][0][i + 4] = (*cube)[YELLOW][1][i];
+		(*cube)[YELLOW][1][i] = (*cube)[YELLOW][2][i + 4];
+		(*cube)[YELLOW][2][i] = (*cube)[YELLOW][2][i + 8];
+		(*cube)[YELLOW][2][i + 4] = (*cube)[YELLOW][1][i + 8];
 	}
-	for (int i = 0; i < 12; i++)
-		(*cube)[YELLOW][2][i] = temp[i];
+	for (int i = 0; i < ROW; i++)
+		for (int j = 0; j < 4; j++)
+			(*cube)[YELLOW][i][j + 8] = temp[(i + 1) * 4 + j - 4];
 }
 
 /**
@@ -90,18 +90,17 @@ void	move_left(char ****cube)
 		}
 	}
 	for (int i = 0; i < 12; i++)
-		temp[i] = (*cube)[BLUE][0][i];
+		temp[i] = (*cube)[BLUE][i / 4][i % 4];
 	for (int i = 0; i < 4; i++)
 	{
-		(*cube)[BLUE][0][i] = (*cube)[BLUE][2][i];
-		(*cube)[BLUE][0][i + 4] = (*cube)[BLUE][1][i];
-		(*cube)[BLUE][1][i] = (*cube)[BLUE][2][i + 4];
-		(*cube)[BLUE][2][i] = (*cube)[BLUE][2][i + 8];
-		(*cube)[BLUE][2][i + 4] = (*cube)[BLUE][1][i + 8];
+		(*cube)[BLUE][0][i] = (*cube)[BLUE][0][i + 8];
+		(*cube)[BLUE][1][i] = (*cube)[BLUE][0][i + 4];
+		(*cube)[BLUE][0][i + 4] = (*cube)[BLUE][1][i + 8];
+		(*cube)[BLUE][0][i + 8] = (*cube)[BLUE][2][i + 8];
+		(*cube)[BLUE][1][i + 8] = (*cube)[BLUE][2][i + 4];
 	}
-	for (int i = 0; i < ROW; i++)
-		for (int j = 0; j < 4; j++)
-			(*cube)[BLUE][i][j + 8] = temp[(i + 1) * 4 + j - 4];
+	for (int i = 0; i < 12; i++)
+		(*cube)[BLUE][2][i] = temp[i];
 }
 
 /**
@@ -125,17 +124,18 @@ void	move_right(char ****cube)
 		}
 	}
 	for (int i = 0; i < 12; i++)
-		temp[i] = (*cube)[GREEN][i / 4][i % 4];
+		temp[i] = (*cube)[GREEN][0][i];
 	for (int i = 0; i < 4; i++)
 	{
-		(*cube)[GREEN][0][i] = (*cube)[GREEN][0][i + 8];
-		(*cube)[GREEN][1][i] = (*cube)[GREEN][0][i + 4];
-		(*cube)[GREEN][0][i + 4] = (*cube)[GREEN][1][i + 8];
-		(*cube)[GREEN][0][i + 8] = (*cube)[GREEN][2][i + 8];
-		(*cube)[GREEN][1][i + 8] = (*cube)[GREEN][2][i + 4];
+		(*cube)[GREEN][0][i] = (*cube)[GREEN][2][i];
+		(*cube)[GREEN][0][i + 4] = (*cube)[GREEN][1][i];
+		(*cube)[GREEN][1][i] = (*cube)[GREEN][2][i + 4];
+		(*cube)[GREEN][2][i] = (*cube)[GREEN][2][i + 8];
+		(*cube)[GREEN][2][i + 4] = (*cube)[GREEN][1][i + 8];
 	}
-	for (int i = 0; i < 12; i++)
-		(*cube)[GREEN][2][i] = temp[i];
+	for (int i = 0; i < ROW; i++)
+		for (int j = 0; j < 4; j++)
+			(*cube)[GREEN][i][j + 8] = temp[(i + 1) * 4 + j - 4];
 }
 
 /**
@@ -154,18 +154,19 @@ void	move_up(char ****cube)
 		(*cube)[PURPLE][2][i] = (*cube)[BLUE][2][i];
 		(*cube)[BLUE][2][i] = (*cube)[YELLOW][0][i];
 		(*cube)[YELLOW][0][i] = temp[i];
-		temp[i] = (*cube)[ORANGE][i / 4][i % 4];
+		temp[i] = (*cube)[ORANGE][0][i];
 	}
 	for (int i = 0; i < 4; i++)
 	{
-		(*cube)[ORANGE][0][i] = (*cube)[ORANGE][0][i + 8];
-		(*cube)[ORANGE][1][i] = (*cube)[ORANGE][0][i + 4];
-		(*cube)[ORANGE][0][i + 4] = (*cube)[ORANGE][1][i + 8];
-		(*cube)[ORANGE][0][i + 8] = (*cube)[ORANGE][2][i + 8];
-		(*cube)[ORANGE][1][i + 8] = (*cube)[ORANGE][2][i + 4];
+		(*cube)[ORANGE][0][i] = (*cube)[ORANGE][2][i];
+		(*cube)[ORANGE][0][i + 4] = (*cube)[ORANGE][1][i];
+		(*cube)[ORANGE][1][i] = (*cube)[ORANGE][2][i + 4];
+		(*cube)[ORANGE][2][i] = (*cube)[ORANGE][2][i + 8];
+		(*cube)[ORANGE][2][i + 4] = (*cube)[ORANGE][1][i + 8];
 	}
-	for (int i = 0; i < 12; i++)
-		(*cube)[ORANGE][2][i] = temp[i];
+	for (int i = 0; i < ROW; i++)
+		for (int j = 0; j < 4; j++)
+			(*cube)[ORANGE][i][j + 8] = temp[(i + 1) * 4 + j - 4];
 }
 
 /**
@@ -184,19 +185,18 @@ void	move_down(char ****cube)
 		(*cube)[BLUE][0][i] = (*cube)[PURPLE][0][i];
 		(*cube)[PURPLE][0][i] = (*cube)[GREEN][2][i];
 		(*cube)[GREEN][2][i] = temp[i];
-		temp[i] = (*cube)[RED][0][i];
+		temp[i] = (*cube)[RED][i / 4][i % 4];
 	}
 	for (int i = 0; i < 4; i++)
 	{
-		(*cube)[RED][0][i] = (*cube)[RED][2][i];
-		(*cube)[RED][0][i + 4] = (*cube)[RED][1][i];
-		(*cube)[RED][1][i] = (*cube)[RED][2][i + 4];
-		(*cube)[RED][2][i] = (*cube)[RED][2][i + 8];
-		(*cube)[RED][2][i + 4] = (*cube)[RED][1][i + 8];
+		(*cube)[RED][0][i] = (*cube)[RED][0][i + 8];
+		(*cube)[RED][1][i] = (*cube)[RED][0][i + 4];
+		(*cube)[RED][0][i + 4] = (*cube)[RED][1][i + 8];
+		(*cube)[RED][0][i + 8] = (*cube)[RED][2][i + 8];
+		(*cube)[RED][1][i + 8] = (*cube)[RED][2][i + 4];
 	}
-	for (int i = 0; i < ROW; i++)
-		for (int j = 0; j < 4; j++)
-			(*cube)[RED][i][j + 8] = temp[(i + 1) * 4 + j - 4];
+	for (int i = 0; i < 12; i++)
+		(*cube)[RED][2][i] = temp[i];
 }
 
 /**
